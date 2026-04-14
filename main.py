0c395f7ce3cd135d05d8b532633ef7673fdd31b3
Gilbert <sgil.grane@gmail.com>
2026-04-14 09:28:35 +0200

    Add Search by Main Ingredient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-from data import CATEGORIES, AREAS
+from flask import Flask, render_template, request, flash, redirect, url_for
+from data import CATEGORIES, AREAS, INGREDIENTS
 from flask_bootstrap import Bootstrap5
 from datetime import datetime
 import requests
@@ -14,7 +14,6 @@ COLOR_PALETTE = {
     "pink": "#FFC1DA",
     "yellow": "#F8F8E1",
     "green": "#8ACCD5",
-    "url": "https://colorhunt.co/palette/ff90bbffc1daf8f8e18accd5",
 }
 
 FONTS = {
@@ -22,16 +21,19 @@ FONTS = {
     "body": "Instrument Sans",
 }
 
+API_KEY = os.environ.get("API_KEY")
 
-SEARCH_BY_CATEGORY_URL = "https://www.themealdb.com/api/json/v1/1/filter.php"
-GET_RECIPE_BY_ID = "https://www.themealdb.com/api/json/v1/1/lookup.php"
-GET_RANDOM_MEAL_URL = "https://www.themealdb.com/api/json/v1/1/random.php"
-SEARCH_BY_MAIN_INGREDIENT = "https://www.themealdb.com/api/json/v1/1/filter.php"
-SEARCH_BY_NAME = "https://www.themealdb.com/api/json/v1/1/search.php"
+
+SEARCH_BY_CATEGORY_URL = f"https://www.themealdb.com/api/json/v2/{API_KEY}/filter.php"
+GET_RECIPE_BY_ID = f"https://www.themealdb.com/api/json/v2/{API_KEY}/lookup.php"
+GET_RANDOM_MEAL_URL = f"https://www.themealdb.com/api/json/v2/{API_KEY}/random.php"
+GET_10_RANDOM_MEALS = f"https://www.themealdb.com/api/json/v2/{API_KEY}/randomselection.php"
+SEARCH_BY_MAIN_INGREDIENT = f"https://www.themealdb.com/api/json/v2/{API_KEY}/filter.php"
+SEARCH_BY_NAME = f"https://www.themealdb.com/api/json/v2/{API_KEY}/search.php"
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = os.environ.get("APP_SECRET_KEY")
 
 
 @app.context_processor
@@ -39,20 +41,13 @@ def inject_date():
   return {"date": datetime.now().year}
 
 
+
 @app.route("/")
 def home():
-    example_meals = []
-    for n in range(3):
-        repeat = True
-        example = None
-        while repeat:
-            response = requests.get(url=GET_RANDOM_MEAL_URL)
-            response.raise_for_status()
-            example = response.json()["meals"]
-            if example not in example_meals:
-                repeat = False
-
-        example_meals.append(example)
+    response = requests.get(url=GET_10_RANDOM_MEALS)
+    response.raise_for_status()
+    example_meals = response.json()["meals"]
+    print(example_meals)
 
     return render_template("index.html", example_meals=example_meals)
 
@@ -96,6 +91,31 @@ def search_by(choice):
     meals = response.json()["meals"]
 
     return render_template("meals.html", meals=meals)
+
+
+
+@app.route("/main-ingredient-search", methods=['GET', 'POST'])
+def main_ingredient_search():
+    if request.method == 'POST':
+        user_input = request.form["input"]
+
+        if user_input == "Select":
+            flash("Please, select an ingredient")
+            return redirect(url_for("main_ingredient_search"))
+
+        params = {
+            "i": user_input
+        }
+        response = requests.get(url=SEARCH_BY_MAIN_INGREDIENT, params=params)
+        response.raise_for_status()
+        meals = response.json()["meals"]
+
+        if not meals:
+            flash("There are not recipes with that ingredient, be sure to click an element from the list")
+            return redirect(url_for("main_ingredient_search"))
+
+        return render_template("meals.html", meals=meals)
+    return render_template("dropdown-search.html", ingredients=INGREDIENTS)
 
 
 
