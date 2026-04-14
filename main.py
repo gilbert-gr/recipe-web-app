3683e5d10683ef274efb4ddeacb89ac83342ef9f
Gilbert <sgil.grane@gmail.com>
2026-04-14 10:35:20 +0200

    Add Search by Name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
-from data import CATEGORIES, AREAS, INGREDIENTS
+from data import CATEGORIES, AREAS, INGREDIENTS, RECIPES
 from flask_bootstrap import Bootstrap5
 from datetime import datetime
 import requests
@@ -24,7 +24,7 @@ FONTS = {
 API_KEY = os.environ.get("API_KEY")
 
 
-SEARCH_BY_CATEGORY_URL = f"https://www.themealdb.com/api/json/v2/{API_KEY}/filter.php"
+SEARCH_BY_AREA_OR_CATEGORY_URL = f"https://www.themealdb.com/api/json/v2/{API_KEY}/filter.php"
 GET_RECIPE_BY_ID = f"https://www.themealdb.com/api/json/v2/{API_KEY}/lookup.php"
 GET_RANDOM_MEAL_URL = f"https://www.themealdb.com/api/json/v2/{API_KEY}/random.php"
 GET_10_RANDOM_MEALS = f"https://www.themealdb.com/api/json/v2/{API_KEY}/randomselection.php"
@@ -44,7 +44,7 @@ def inject_date():
 
 @app.route("/")
 def home():
-    response = requests.get(url=GET_10_RANDOM_MEALS)
+    response = requests.get(url=GET_10_RANDOM_MEALS, timeout=5)
     response.raise_for_status()
     example_meals = response.json()["meals"]
     print(example_meals)
@@ -61,7 +61,7 @@ def about():
 
 @app.route("/random-meal")
 def get_random_meal():
-    response = requests.get(url=GET_RANDOM_MEAL_URL)
+    response = requests.get(url=GET_RANDOM_MEAL_URL, timeout=5)
     response.raise_for_status()
 
     meal = response.json()["meals"]
@@ -86,7 +86,7 @@ def search_by(choice):
     params = {
         method: choice,
     }
-    response = requests.get(url=SEARCH_BY_CATEGORY_URL, params=params)
+    response = requests.get(url=SEARCH_BY_AREA_OR_CATEGORY_URL, params=params, timeout=5)
     response.raise_for_status()
     meals = response.json()["meals"]
 
@@ -106,7 +106,7 @@ def main_ingredient_search():
         params = {
             "i": user_input
         }
-        response = requests.get(url=SEARCH_BY_MAIN_INGREDIENT, params=params)
+        response = requests.get(url=SEARCH_BY_MAIN_INGREDIENT, params=params, timeout=5)
         response.raise_for_status()
         meals = response.json()["meals"]
 
@@ -119,12 +119,41 @@ def main_ingredient_search():
 
 
 
+@app.route("/name-search", methods=['GET', 'POST'])
+def name_search():
+    if request.method == 'POST':
+        user_input = request.form["input"]
+
+
+        if user_input == "Select":
+            flash("Please, select a meal")
+            return redirect(url_for("name_search"))
+
+        try:
+            meal_id = RECIPES[user_input]
+        except KeyError:
+            flash("Click an element from the list")
+            return redirect(url_for("name_search"))
+
+        params = {
+            "i": meal_id,
+        }
+        response = requests.get(url=GET_RECIPE_BY_ID, params=params, timeout=5)
+        response.raise_for_status()
+
+        meal = response.json()["meals"]
+        return render_template("recipe.html", meal=meal)
+
+    return render_template("dropdown-search.html", recipes=RECIPES)
+
+
+
 @app.route("/get-recipe/<int:meal_id>")
 def get_recipe(meal_id):
     params = {
         "i": meal_id,
     }
-    response = requests.get(url=GET_RECIPE_BY_ID, params=params)
+    response = requests.get(url=GET_RECIPE_BY_ID, params=params, timeout=5)
     response.raise_for_status()
 
     meal = response.json()["meals"]
